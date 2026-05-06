313bce4af3f15ed38a5fa98049a6fb5d4d382c63
mdegs02 <mdegs02@educastur.es>
2026-05-06 15:11:26 +0200

    Añadida ruta principal

---
+++ src/main/java/es/marian/backendalumnos/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package es.marian.backendalumnos.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "Backend funcionando correctamente ";
+    }
+}
